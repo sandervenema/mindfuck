82dbaec97e38ad671c97b4ee2eaf2c18e70c256b
Sander Venema <sander@sandervenema.ch>
2016-10-13 17:04:10 +0200

    Changed long switch statement for a map.
    Shorter, clearer code. tokenize() maps chars to Tokens, so it makes more
    sense to use a std::map.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,36 +1,26 @@
+#include <map>
+
 #include "tokenizer.hpp"
 
 std::vector<Token> tokenize(std::string input)
 {
+	std::map<char, Token> map = {
+		{'>', Token::INCR_PTR},
+		{'<', Token::DECR_PTR},
+		{'+', Token::INCR},
+		{'-', Token::DECR},
+		{'.', Token::OUTPUT},
+		{',', Token::INPUT},
+		{'[', Token::JEZ},
+		{']', Token::JNZ}
+	};
+
 	std::vector<char> chars(input.begin(), input.end());
 	std::vector<Token> tokens;
 
 	for (auto &c : chars) {
-		switch (c) {
-		case '>':
-			tokens.push_back(Token::INCR_PTR);
-			break;
-		case '<':
-			tokens.push_back(Token::DECR_PTR);
-			break;
-		case '+':
-			tokens.push_back(Token::INCR);
-			break;
-		case '-':
-			tokens.push_back(Token::DECR);
-			break;
-		case '.':
-			tokens.push_back(Token::OUTPUT);
-			break;
-		case ',':
-			tokens.push_back(Token::INPUT);
-			break;
-		case '[':
-			tokens.push_back(Token::JEZ);
-			break;
-		case ']':
-			tokens.push_back(Token::JNZ);
-			break;
+		if (map.find(c) != map.end()) {
+			tokens.push_back(map[c]);
 		}
 	}
 
